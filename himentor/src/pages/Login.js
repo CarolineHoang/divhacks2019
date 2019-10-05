08aee5cd2bbf3ddd21df5a7347d6b74e4e5d29fc
keyuyan <ke.yuyan@columbia.edu>
2019-10-05 19:33:21 -0400

    login page without button

--- himentor/src/pages/Login.js
+++ himentor/src/pages/Login.js
@@ -1,28 +1,52 @@
-import React, { Component } from 'react';
 
+import React, { Component } from 'react';
 
 
 class Login extends Component {
     constructor(){
         super();
+        this.state = {
+                email: ' ' ,
+                password: ' '
+            };
     }
-    // this.state = {
-    //   };
+    
+
+    changeHandlerName = event => {
+        this.setState({ 
+            email:event.target.email
+        });
+    }
+
+    changeHandlerPassword = event => {
+        this.setState({ 
+            password:event.target.password
+        });
+    }
 
 
-
-
-render (){
+render () {
     return(
-        <div>
-            lalalaldknadvjnaov
+        <div className="Login">
+            <form>
+                Email 
+                <input type="email" 
+                 name="email"   
+                 value={this.state.email} 
+                 onChange={this.changeHandlerName} 
+                />
 
+                Password
+                <input type="passwordd" 
+                 name="password"   
+                 value={this.state.password} 
+                 onChange={this.changeHandlerPassword} 
+                />
+
+            </form>
         </div>
 
     )
-
-
-
 
 };
 }
